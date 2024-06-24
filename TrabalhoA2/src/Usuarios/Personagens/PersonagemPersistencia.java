8f54b9a862edda9af4d3bc8647d5b83e44beb0cc
dudu025 <dudueduardorodrigo@gmail.com>
2024-06-24 09:51:05 -0300

    atualizacao PersonagemPersistencia

--- TrabalhoA2/src/Usuarios/Personagens/PersonagemPersistencia.java
+++ TrabalhoA2/src/Usuarios/Personagens/PersonagemPersistencia.java
@@ -9,7 +9,7 @@ public class PersonagemPersistencia {
     public static void salvarHeroi(String nomeUsuario, Heroi heroi) throws IOException {
         String nomeArquivo = nomeUsuario + "_personagem.txt";
         try (FileWriter fWriter = new FileWriter(nomeArquivo, true); // Append mode
-             BufferedWriter bWriter = new BufferedWriter(fWriter)) {
+                BufferedWriter bWriter = new BufferedWriter(fWriter)) {
             bWriter.write(heroi.toString() + "\n");
         }
     }
@@ -18,7 +18,7 @@ public class PersonagemPersistencia {
         String nomeArquivo = nomeUsuario + "_personagem.txt";
         List<Heroi> listaHerois = new ArrayList<>();
         try (FileReader frReader = new FileReader(nomeArquivo);
-             BufferedReader bReader = new BufferedReader(frReader)) {
+                BufferedReader bReader = new BufferedReader(frReader)) {
             String linha;
             while ((linha = bReader.readLine()) != null) {
                 Heroi heroi = new Heroi();
@@ -39,5 +39,33 @@ public class PersonagemPersistencia {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+    }
+
+    public static void atualizarArquivo(String nomeUsuario, List<Heroi> listaHeroisAtualizada) throws IOException {
+        String nomeArquivo = nomeUsuario + "_personagem.txt";
+        // Sobrescreve o arquivo com a lista de heróis atualizada
+        try (FileWriter fWriter = new FileWriter(nomeArquivo);
+                BufferedWriter bWriter = new BufferedWriter(fWriter)) {
+            for (Heroi heroi : listaHeroisAtualizada) {
+                bWriter.write(heroi.toString() + "\n");
+            }
+        }
+    }
+    
+    public static void atualizarHeroi(String nomeUsuario, Heroi heroiAtualizado) throws Exception {
+        List<Heroi> listaHerois = lerHerois(nomeUsuario);
+        boolean encontrado = false;
+        for (int i = 0; i < listaHerois.size(); i++) {
+            Heroi heroi = listaHerois.get(i);
+            if (heroi.getNome().equals(heroiAtualizado.getNome())) {
+                listaHerois.set(i, heroiAtualizado);
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            throw new Exception("Herói com nome " + heroiAtualizado.getNome() + " não encontrado.");
+        }
+        atualizarArquivo(nomeUsuario, listaHerois);
     }
 }
